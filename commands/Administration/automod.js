e6c3185403d1a971c6bc60591d33d09d29aff069
Androz2091 <androz2091@gmail.com>
2020-01-18 14:22:39 +0100

    i18n(automod): switch to new version

--- commands/Administration/automod.js
+++ commands/Administration/automod.js
@@ -6,9 +6,9 @@ class Automod extends Command {
     constructor (client) {
         super(client, {
             name: "automod",
-            description: (language) => language.get("AUTOMOD_DESCRIPTION"),
-            usage: (language) => language.get("AUTOMOD_USAGE"),
-            examples: (language) => language.get("AUTOMOD_EXAMPLES"),
+            description: (language) => language.t("cmd.automod.self.description"),
+            usage: (language) => language.t("cmd.automod.self.usage"),
+            examples: (language) => language.t("cmd.automod.self.examples"),
             dirname: __dirname,
             enabled: true,
             guildOnly: true,
@@ -25,26 +25,30 @@ class Automod extends Command {
 
         let status = args[0];
         if(!status || (status !== "on" && status !== "off")){
-            return message.channel.send(message.language.get("AUTOMOD_ERR_STATUS"));
+            return message.channel.send(message.language.t("cmd.automod.errors.status"));
         }
 
         if(status === "on"){
             data.guild.plugins.automod = { enabled: true, ignored: [] };
             data.guild.markModified("plugins.automod");
             data.guild.save();
-            message.channel.send(message.language.get("AUTOMOD_SUCCESS_ENABLED", data.guild.prefix));
+            message.channel.send(message.language.t("cmd.automod.enabled", {
+                prefix: data.guild.prefix
+            }));
         } else if (status === "off"){
             if(message.mentions.channels.filter((ch) => ch.type === "text" && ch.guild.id === message.guild.id).first()){
                 let channel = message.mentions.channels.first();
                 data.guild.plugins.automod.ignored.push(channel);
                 data.guild.markModified("plugins.automod");
                 data.guild.save();
-                message.channel.send(message.language.get("AUTOMOD_SUCCESS_DISABLED_CHANNEL", channel));
+                message.channel.send(message.language.get("cmd.automod.disabledChannel", {
+                    channel
+                }));
             } else {
                 data.guild.plugins.automod = { enabled: false, ignored: [] };
                 data.guild.markModified("plugins.automod");
                 data.guild.save();
-                message.channel.send(message.language.get("AUTOMOD_SUCCESS_DISABLED"));
+                message.channel.send(message.language.get("cmd.automod.disabled"));
             }
         }
     }
